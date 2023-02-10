25180739c75ed63cfd86305524033eb5af4c7d88
Siddheshwar Vikram Ghumare <siddheshwar.ghumare@ptw.com>
2023-02-10 18:30:25 +0530

    No BS TS Challenge #2

---
+++ challenge-2/index.ts
@@ -0,0 +1,17 @@
+function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
+  items.reduce((a, v) => {
+    forEachFunc(v);
+    return undefined;
+  }, undefined);
+}
+myForEach(["a", "b", "c"], (v) => console.log(`forEach ${v}`));
+
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+  return items.reduce((a: T[], v) => (filterFunc(v) ? [...a, v] : a), []);
+}
+console.log(myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0));
+
+function myMap<T, K>(items: T[], mapFunc: (v: T) => K): K[] {
+  return items.reduce((a, v) => [...a, mapFunc(v)], [] as K[]);
+}
+console.log(myMap([1, 2, 3, 4, 5, 6, 7, 8], (v) => (v * 10).toString()));
