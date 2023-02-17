ab2fb51aa7d4718a0f556b3bf35f6a8efd2d52b4
Siddheshwar Vikram Ghumare <siddheshwar.ghumare@ptw.com>
2023-02-17 18:25:35 +0530

    No BS TS #13 - Generics in Typescript Classes

--- database.ts
+++ database.ts
@@ -1,6 +1,6 @@
-interface Database {
-  get(id: string): string;
-  set(id: string, value: string): void;
+interface Database<T, K> {
+  get(id: K): T;
+  set(id: K, value: T): void;
 }
 
 interface Persistable {
@@ -8,17 +8,22 @@ interface Persistable {
   restoreFromString(storedState: string): void;
 }
 
-class InMemoryDatabase implements Database {
-  protected db: Record<string, string> = {};
-  get(id: string): string {
+type DBKeyType = string | number | symbol;
+
+class InMemoryDatabase<T, K extends DBKeyType> implements Database<T, K> {
+  protected db: Record<K, T> = {} as Record<K, T>;
+  get(id: K): T {
     return this.db[id];
   }
-  set(id: string, value: string): void {
+  set(id: K, value: T): void {
     this.db[id] = value;
   }
 }
 
-class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
+class PersistentMemoryDB<T, K extends DBKeyType>
+  extends InMemoryDatabase<T, K>
+  implements Persistable
+{
   saveToString(): string {
     return JSON.stringify(this.db);
   }
@@ -27,15 +32,14 @@ class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
   }
 }
 
-const myDB = new PersistentMemoryDB();
-myDB.set("foo", "bar");
+const myDB = new PersistentMemoryDB<number, string>();
+myDB.set("foo", 22);
 // myDB.db["foo"] = "baz";
 console.log(myDB.get("foo"));
 const saved = myDB.saveToString();
-myDB.set("foo", "db1 - bar");
+myDB.set("foo", 23);
 console.log(myDB.get("foo"));
 
-
-const myDB2 = new PersistentMemoryDB();
+const myDB2 = new PersistentMemoryDB<number, string>();
 myDB2.restoreFromString(saved);
 console.log(myDB2.get("foo"));
